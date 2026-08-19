3e40c24dfa71fe27838230f17b6c9ffe5e4d82c4
PhuHV <hoangvanphu102@protonmail.com>
2026-08-19 10:50:57 +0700

    refactor: Remove unused import from TrackedVideo entity

--- src/main/java/com/example/autoevaluation/entity/autoeval/TrackedVideo.java
+++ src/main/java/com/example/autoevaluation/entity/autoeval/TrackedVideo.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.*;
 import lombok.Data;
